56b7baa2f2461dd1db6e099eb857726a37f80d8c
李连成 <liancheng.li@yunhutech.com>
2020-03-07 17:39:01 +0800

    function is ok

--- myownWheels/tabs/main.ts
+++ myownWheels/tabs/main.ts
@@ -1,3 +1,16 @@
-document.getElementById('tab1').addEventListener('click', () => {
-    console.log('1')
+let tab1 = document.getElementById('tab1');
+let tab2 = document.getElementById('tab2');
+let box1 = document.getElementById('box1')
+let box2 = document.getElementById('box2')
+tab1.addEventListener('click', () => {
+    tab1.classList.add('active')
+    tab2.classList.remove('active')
+    box2.classList.add('boxActive')
+    box1.classList.remove('boxActive')
+}, false)
+tab2.addEventListener('click', () => {
+    tab2.classList.add('active')
+    tab1.classList.remove('active')
+    box1.classList.add('boxActive')
+    box2.classList.remove('boxActive')
 }, false)
